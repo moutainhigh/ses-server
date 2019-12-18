1ab6d4e9eba940a71bdf092827930d3365c5c697
alexrede <alex@redescooter.com>
2019-12-18 15:23:33 +0800

    解决Test 导包问题

--- ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/dao/base/OpeCustomerContactMapper.java
+++ ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/dao/base/OpeCustomerContactMapper.java
@@ -1,9 +1,9 @@
 package com.redescooter.ses.web.ros.dao.base;
 
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.redescooter.ses.web.ros.dm.OpeCustomerContact;
+import org.jboss.logging.Param;
+
 import java.util.List;
-import org.apache.ibatis.annotations.Param;
 
 public interface OpeCustomerContactMapper extends BaseMapper<OpeCustomerContact> {
     int updateBatch(List<OpeCustomerContact> list);
