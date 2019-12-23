e5a0e4e8f70a856aafab80526448aebdd1a60da2
李加廷 <lijiating621@gmail.com>
2019-12-24 02:05:07 +0800

    集成txlcn

---
+++ ses-starter/ses-starter-txlcn/src/test/java/com/redescooter/ses/starter/txlcn/SesStarterTxlcnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redescooter.ses.starter.txlcn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SesStarterTxlcnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
