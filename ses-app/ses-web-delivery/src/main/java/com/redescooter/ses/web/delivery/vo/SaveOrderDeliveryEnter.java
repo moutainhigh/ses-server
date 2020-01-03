95319df402a351517cea17a34844cf537c395aa4
alexrede <alex@redescooter.com>
2020-01-03 19:50:38 +0800

    delivery-web 异常补充完成

--- ses-app/ses-web-delivery/src/main/java/com/redescooter/ses/web/delivery/vo/SaveOrderDeliveryEnter.java
+++ ses-app/ses-web-delivery/src/main/java/com/redescooter/ses/web/delivery/vo/SaveOrderDeliveryEnter.java
@@ -1,9 +1,17 @@
 package com.redescooter.ses.web.delivery.vo;
 
+import com.redescooter.ses.api.common.annotation.MaximumLength;
+import com.redescooter.ses.api.common.annotation.MinimumLength;
+import com.redescooter.ses.api.common.annotation.NotNull;
 import com.redescooter.ses.api.common.vo.base.GeneralEnter;
+import com.redescooter.ses.web.delivery.exception.ValidationExceptionCode;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 /**
  * @author Mr.lijiating
@@ -24,27 +32,43 @@ public class SaveOrderDeliveryEnter extends GeneralEnter {
     private Long id;
 
     @ApiModelProperty(value = "司机主键")
+    @NotNull(code = ValidationExceptionCode.ID_IS_EMPTY, message = "Id 为空")
     private Long driverId;
 
     @ApiModelProperty(value = "收件人")
+    @NotNull(code = ValidationExceptionCode.RECIPIENT_IS_EMPTY, message = "收件人为空")
+    @MinimumLength(code = ValidationExceptionCode.RECIPIENT_IS_UNAVAILABLE, message = "姓字符长度为2-20字符")
+    @MaximumLength(code = ValidationExceptionCode.RECIPIENT_IS_UNAVAILABLE, message = "姓字符长度为2-20字符")
     private String recipient;
 
     @ApiModelProperty(value = "收件人邮箱")
+    @NotNull(code = ValidationExceptionCode.RECIPIENT_IS_EMPTY, message = "收件人为空")
+    @MinimumLength(code = ValidationExceptionCode.EMAIL_IS_UNAVAILABLE, message = "邮箱字符长度为2-20字符")
+    @MaximumLength(code = ValidationExceptionCode.EMAIL_IS_UNAVAILABLE, message = "邮箱字符长度为2-20字符")
     private String recipientEmail;
 
     @ApiModelProperty(value = "手机国家区号")
+    @NotNull(code = ValidationExceptionCode.COUNTRY_CODE_IS_EMPTY, message = "手机国家区号为空")
     private String countryCode;
 
     @ApiModelProperty(value = "收件人电话")
+    @NotNull(code = ValidationExceptionCode.PHONE_IS_EMPTY, message = "电话为空")
+    @MinimumLength(code = ValidationExceptionCode.PHONE_IS_UNAVAILABLE, message = "电话字符长度为2-20字符")
+    @MaximumLength(code = ValidationExceptionCode.PHONE_IS_UNAVAILABLE, message = "电话字符长度为2-20字符")
     private String recipientTel;
 
     @ApiModelProperty(value = "收件人地址")
+    @NotNull(code = ValidationExceptionCode.ADDRESS_IS_EMPTY, message = "收件人地址为空")
+    @MinimumLength(code = ValidationExceptionCode.ADDRESS_IS_UNAVAILABLE, message = "收件人地址字符长度为2-20字符")
+    @MaximumLength(code = ValidationExceptionCode.ADDRESS_IS_UNAVAILABLE, message = "收件人地址字符长度为2-20字符")
     private String recipientAddress;
 
     @ApiModelProperty(value = "经度")
+    @NotNull(code = ValidationExceptionCode.LNG_IS_EMPTY, message = "经度为空")
     private String longitude;
 
     @ApiModelProperty(value = "纬度")
+    @NotNull(code = ValidationExceptionCode.LAT_IS_EMPTY, message = "纬度为空")
     private String latitude;
 
     @ApiModelProperty(value = "门牌信息")
