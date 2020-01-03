94029f6ad75287131ed7bc3f391e8079c12236a2
alexrede <alex@redescooter.com>
2020-01-03 18:54:04 +0800

    修改司机状态统计

--- ses-app/ses-web-delivery/src/main/java/com/redescooter/ses/web/delivery/exception/ValidationExceptionCode.java
+++ ses-app/ses-web-delivery/src/main/java/com/redescooter/ses/web/delivery/exception/ValidationExceptionCode.java
@@ -21,6 +21,8 @@ public interface ValidationExceptionCode {
     int BUSSINESS_OBJ_IS_EMPTY = 10005;
     // 图片为空
     int PICTURE_IS_EMPTY = 10006;
+    // 邮箱为空
+    int EMAIL_IS_EMPTY = 10007;
 
     //10030 之后业务异常
 
@@ -28,9 +30,30 @@ public interface ValidationExceptionCode {
     int FIRST_NAME_IS_EMPTY = 10031;
     // 名不可用
     int FIRST_NAME_IS_UNAVAILABLE = 10032;
-
-
+    // 姓为空
     int LAST_NAME_IS_EMPTY = 10032;
-
+    // 姓不可用
     int LAST_NAME_IS_UNAVAILABLE = 10033;
+    // 性别为空
+    int GENDER_IS_EMPTY = 10034;
+    // 手机区号为空
+    int COUNTRY_CODE_IS_EMPTY = 10035;
+    // 手机号为空
+    int PHONE_IS_EMPTY = 10036;
+    // 手机号不可用
+    int PHONE_IS_UNAVAILABLE = 10037;
+    // 邮箱不可用
+    int EMAIL_IS_UNAVAILABLE = 10038;
+    // 地址为空
+    int ADDRESS_IS_EMPTY = 10039;
+    // 地址不可用
+    int ADDRESS_IS_UNAVAILABLE = 10040;
+    // 生日为空
+    int BIRTHDAY_IS_EMPTY = 10041;
+    // 驾驶证为空
+    int DRIVER_LICENSE_IS_EMPTY = 10042;
+    // 驾驶证 正面 为空
+    int DRIVER_LICENSE_UPANNEX_IS_EMPTY = 10043;
+    // 驾驶证反面为空
+    int DRIVER_LICENSE_DOWNANNEX_IS_EMPTY = 10044;
 }
--- ses-app/ses-web-delivery/src/main/java/com/redescooter/ses/web/delivery/vo/SaveDriverEnter.java
+++ ses-app/ses-web-delivery/src/main/java/com/redescooter/ses/web/delivery/vo/SaveDriverEnter.java
@@ -48,33 +48,48 @@ public class SaveDriverEnter extends GeneralEnter {
     private String driverLastName;
 
     @ApiModelProperty(value = "司机性别")
+    @NotNull(code = ValidationExceptionCode.GENDER_IS_EMPTY, message = "性别为空")
     private String gender;
 
     @ApiModelProperty(value = "手机区号")
+    @NotNull(code = ValidationExceptionCode.COUNTRY_CODE_IS_EMPTY, message = "手机区号为空")
     private String countryCodel;
 
     @ApiModelProperty(value = "司机手机号")
+    @NotNull(code = ValidationExceptionCode.PHONE_IS_EMPTY, message = "手机号为空")
+    @MinimumLength(code = ValidationExceptionCode.PHONE_IS_UNAVAILABLE, message = "手机号字符长度为2-20字符")
+    @MaximumLength(code = ValidationExceptionCode.PHONE_IS_UNAVAILABLE, message = "手机号字符长度为2-20字符")
     private String driverPhone;
 
     @ApiModelProperty(value = "司机邮箱")
+    @NotNull(code = ValidationExceptionCode.EMAIL_IS_EMPTY, message = "邮箱为空")
+    @MinimumLength(code = ValidationExceptionCode.EMAIL_IS_UNAVAILABLE, message = "邮箱字符长度为2-20字符")
+    @MaximumLength(code = ValidationExceptionCode.EMAIL_IS_UNAVAILABLE, message = "邮箱字符长度为2-20字符")
     private String email;
 
     @ApiModelProperty(value = "司机地址")
+    @NotNull(code = ValidationExceptionCode.ADDRESS_IS_EMPTY, message = "地址为空")
+    @MinimumLength(code = ValidationExceptionCode.ADDRESS_IS_UNAVAILABLE, message = "地址字符长度为2-20字符")
+    @MaximumLength(code = ValidationExceptionCode.ADDRESS_IS_UNAVAILABLE, message = "地址字符长度为2-20字符")
     private String address;
 
     @ApiModelProperty(value = "司机生日")
+    @NotNull(code = ValidationExceptionCode.BIRTHDAY_IS_EMPTY, message = "生日为空")
     private String birthday;
 
     @ApiModelProperty(value = "证件类型1身份证，2驾驶证，3护照")
     private String certificateType;
 
     @ApiModelProperty(value = "驾驶证")
+    @NotNull(code = ValidationExceptionCode.DRIVER_LICENSE_IS_EMPTY, message = "驾驶证为空")
     private String driverLicense;
 
     @ApiModelProperty(value = "驾驶证附件上")
+    @NotNull(code = ValidationExceptionCode.DRIVER_LICENSE_UPANNEX_IS_EMPTY, message = "驾驶证附件为空")
     private String driverLicenseUpAnnex;
 
     @ApiModelProperty(value = "驾驶证附件下")
+    @NotNull(code = ValidationExceptionCode.DRIVER_LICENSE_DOWNANNEX_IS_EMPTY, message = "生日为空")
     private String driverLicenseDownAnnex;
 
 }
