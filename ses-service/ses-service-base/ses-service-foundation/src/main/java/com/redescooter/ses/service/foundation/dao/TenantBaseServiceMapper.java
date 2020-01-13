41d8b6320ab9c088f340480d1fa9f1c324345738
alex <alex@redescooter.com>
2020-01-13 15:16:49 +0800

    Saas 创建司机账户 租户司机数量进累加

---
+++ ses-service/ses-service-base/ses-service-foundation/src/main/java/com/redescooter/ses/service/foundation/dao/TenantBaseServiceMapper.java
@@ -0,0 +1,16 @@
+package com.redescooter.ses.service.foundation.dao;
+
+import com.redescooter.ses.api.common.vo.base.GeneralEnter;
+
+/**
+ * @ClassName:TenantBaseServiceMapper
+ * @description: TenantBaseServiceMapper
+ * @author: Alex
+ * @Version：1.3
+ * @create: 2020/01/13 15:02
+ */
+public interface TenantBaseServiceMapper {
+
+
+    void updateDriverCount(GeneralEnter enter);
+}
