80bca05608e6df522fdb0e77296466c871c42333
李加廷 <lijiating621@gmail.com>
2020-01-13 18:57:33 +0800

    架构redis 优化

---
+++ ses-app/ses-app-common/src/main/java/com/redescooter/ses/app/common/controller/BaseController.java
@@ -0,0 +1,53 @@
+package com.redescooter.ses.app.common.controller;
+
+import cn.hutool.core.date.DateUtil;
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+
+import java.beans.PropertyEditorSupport;
+import java.sql.Timestamp;
+import java.util.Date;
+
+/**
+ * @author Mr.lijiating
+ * @version V1.0
+ * @Date: 13/1/2020 5:43 下午
+ * @ClassName: BaseController
+ * @Function: TODO
+ */
+public class BaseController {
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        // String类型转换，将所有传递进来的String进行HTML编码，防止XSS攻击
+        binder.registerCustomEditor(String.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) {
+                setValue(text == null ? null : StringEscapeUtils.escapeHtml4(text.trim()));
+            }
+            @Override
+            public String getAsText() {
+                Object value = getValue();
+                return value != null ? value.toString() : "";
+            }
+        });
+
+        // Date 类型转换
+        binder.registerCustomEditor(Date.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) {
+                setValue(DateUtil.parseDate(text));
+            }
+        });
+
+        // Timestamp 类型转换
+        binder.registerCustomEditor(Timestamp.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) {
+                Date date = DateUtil.parseDate(text);
+                setValue(date==null?null:new Timestamp(date.getTime()));
+            }
+        });
+    }
+
+}
