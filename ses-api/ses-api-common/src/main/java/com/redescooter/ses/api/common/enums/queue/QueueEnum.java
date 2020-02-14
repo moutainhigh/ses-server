ea6c484dac3406ea315a027af1c7330e9025f42a
李加廷 <lijiating621@gmail.com>
2020-02-14 14:11:16 +0800

    添加消息队列枚举

---
+++ ses-api/ses-api-common/src/main/java/com/redescooter/ses/api/common/enums/queue/QueueEnum.java
@@ -0,0 +1,38 @@
+package com.redescooter.ses.api.common.enums.queue;
+
+/**
+ * @author Mr.lijiating
+ * @version V1.0
+ * @Date: 14/2/2020 1:58 下午
+ * @ClassName: QueueEnum
+ * @Function: 消息队列枚举配置
+ */
+public enum QueueEnum {
+    /**
+     * 消息通知队列
+     */
+    QUEUE_ORDER_CANCEL("mall.order.direct", "mall.order.cancel", "mall.order.cancel"),
+    /**
+     * 消息通知ttl队列
+     */
+    QUEUE_TTL_ORDER_CANCEL("mall.order.direct.ttl", "mall.order.cancel.ttl", "mall.order.cancel.ttl");
+
+    /**
+     * 交换名称
+     */
+    private String exchange;
+    /**
+     * 队列名称
+     */
+    private String name;
+    /**
+     * 路由键
+     */
+    private String routeKey;
+
+    QueueEnum(String exchange, String name, String routeKey) {
+        this.exchange = exchange;
+        this.name = name;
+        this.routeKey = routeKey;
+    }
+}
