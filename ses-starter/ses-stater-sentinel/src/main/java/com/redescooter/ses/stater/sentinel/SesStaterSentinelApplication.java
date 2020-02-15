8ddc4177a5ebf2051dc51011c6b4431a075206cf
李加廷 <lijiating621@gmail.com>
2020-02-15 23:34:50 +0800

    组件pom文件 修改

--- ses-starter/ses-stater-sentinel/src/main/java/com/redescooter/ses/stater/sentinel/SesStaterSentinelApplication.java
+++ ses-starter/ses-stater-sentinel/src/main/java/com/redescooter/ses/stater/sentinel/SesStaterSentinelApplication.java
@@ -1,13 +1,13 @@
-package com.redescooter.ses.stater.sentinel;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class SesStaterSentinelApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SesStaterSentinelApplication.class, args);
-	}
-
-}
+//package com.redescooter.ses.stater.sentinel;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class SesStaterSentinelApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(SesStaterSentinelApplication.class, args);
+//	}
+//
+//}
