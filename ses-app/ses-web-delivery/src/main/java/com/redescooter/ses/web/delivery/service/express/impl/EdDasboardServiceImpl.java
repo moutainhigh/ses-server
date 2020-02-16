05deb46699752a85d1a6526aaaa9a3b31963c9ad
李加廷 <lijiating621@gmail.com>
2020-02-16 17:39:03 +0800

    新增快递仪表盘 接口

--- ses-app/ses-web-delivery/src/main/java/com/redescooter/ses/web/delivery/service/express/impl/EdDasboardServiceImpl.java
+++ ses-app/ses-web-delivery/src/main/java/com/redescooter/ses/web/delivery/service/express/impl/EdDasboardServiceImpl.java
@@ -7,25 +7,24 @@ import com.redescooter.ses.api.common.vo.base.GeneralEnter;
 import com.redescooter.ses.api.common.vo.base.IdEnter;
 import com.redescooter.ses.api.foundation.service.base.TenantBaseService;
 import com.redescooter.ses.api.foundation.vo.tenant.QueryTenantResult;
+import com.redescooter.ses.tool.utils.DateUtil;
 import com.redescooter.ses.web.delivery.dao.EdDasboardServiceMapper;
+import com.redescooter.ses.web.delivery.dao.OrderStatisticsServiceMapper;
 import com.redescooter.ses.web.delivery.dm.CorTenantScooter;
+import com.redescooter.ses.web.delivery.exception.ExceptionCodeEnums;
+import com.redescooter.ses.web.delivery.exception.SesWebDeliveryException;
 import com.redescooter.ses.web.delivery.service.base.CorTenantScooterService;
 import com.redescooter.ses.web.delivery.service.express.EdDasboardService;
-import com.redescooter.ses.web.delivery.vo.ScooterMapResult;
-import com.redescooter.ses.web.delivery.vo.ScooterRideDataResult;
-import com.redescooter.ses.web.delivery.vo.TopTenEnter;
-import com.redescooter.ses.web.delivery.vo.TopTenResult;
+import com.redescooter.ses.web.delivery.vo.*;
 import com.redescooter.ses.web.delivery.vo.edorder.ExpressOrderMapResult;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.dubbo.config.annotation.Reference;
 import org.apache.dubbo.config.annotation.Service;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class EdDasboardServiceImpl implements EdDasboardService {
@@ -33,6 +32,9 @@ public class EdDasboardServiceImpl implements EdDasboardService {
     private EdDasboardServiceMapper edDasboardServiceMapper;
     @Autowired
     private CorTenantScooterService tenantScooterService;
+
+    @Autowired
+    private OrderStatisticsServiceMapper orderStatisticsServiceMapper;
 
     @Reference
     private TenantBaseService tenantBaseService;
@@ -132,5 +134,144 @@ public class EdDasboardServiceImpl implements EdDasboardService {
                 .tenantLng(tenant.getLongitude() == null ? String.valueOf(BigDecimal.ZERO) : String.valueOf(tenant.getLongitude()))
                 .scooterMapResultList(scooterMapResultList)
                 .build();
+    }
+
+    /**
+     * 快递仪表盘订单柱状图
+     *
+     * @param enter
+     * @return
+     */
+    @Override
+    public DeliveryChartListResult eDDeliveryChartList(DeliveryChartEnter enter) {
+        Map<String, DeliveryChartResult> map = new LinkedHashMap<>();
+        List<DeliveryChartResult> deliveryChartResults = new ArrayList<>();
+        Double max = 0.00, avg = 0.00, min = 0.00;
+
+        //天数
+        int heavens = enter.getHeavens() == 0 ? 1 : enter.getHeavens();
+        enter.setHeavens(heavens);
+        enter.setDateTimes(enter.getDateTimes() == null ? new Date() : enter.getDateTimes());
+        switch (heavens) {
+            case 1:
+                //今日Today（单位为小时，显示今日配送数据）
+                DeliveryChartDto dateTimeParmToday = new DeliveryChartDto();
+
+                BeanUtils.copyProperties(enter, dateTimeParmToday);
+                dateTimeParmToday.setDateTime(enter.getDateTimes());
+                deliveryChartResults = orderStatisticsServiceMapper.eDDliveryChartToday(dateTimeParmToday);
+                break;
+
+            case 7:
+                //近七日<7Day（单位为日，显示近7天配送数据，点击某一日可查看该日数据，并且时间筛选变更为返回）
+                Date start7 = DateUtil.addDays(enter.getDateTimes(), -7);
+                DeliveryChartDto dateTimeParm7 = new DeliveryChartDto();
+
+                BeanUtils.copyProperties(enter, dateTimeParm7);
+                dateTimeParm7.setEndDateTime(enter.getDateTimes());
+                dateTimeParm7.setStartDateTime(start7);
+
+                deliveryChartResults = orderStatisticsServiceMapper.eDDeliveryChart7Day(dateTimeParm7);
+                break;
+
+            case 30:
+                //近30日<30Day（单位为日，显示近30天配送数据，点击某一日可查看该日数据，并且时间筛选变更为返回）
+                Date start30 = DateUtil.addDays(enter.getDateTimes(), -30);
+                DeliveryChartDto dateTimeParm30 = new DeliveryChartDto();
+
+                BeanUtils.copyProperties(enter, dateTimeParm30);
+                dateTimeParm30.setEndDateTime(enter.getDateTimes());
+                dateTimeParm30.setStartDateTime(start30);
+                deliveryChartResults = orderStatisticsServiceMapper.deliveryChart30Day(dateTimeParm30);
+                deliveryChartResults = orderStatisticsServiceMapper.eDDeliveryChart30Day(dateTimeParm30);
+                break;
+
+            case 365:
+                //年Year（单位为月，显示截止至今所有数据，点击某一月可查看该月数据，点击某一日可查看该日数据
+                Date start365 = DateUtil.addDays(enter.getDateTimes(), -365);
+                DeliveryChartDto dateTimeParm365 = new DeliveryChartDto();
+
+                BeanUtils.copyProperties(enter, dateTimeParm365);
+                dateTimeParm365.setEndDateTime(enter.getDateTimes());
+                dateTimeParm365.setStartDateTime(start365);
+                deliveryChartResults = orderStatisticsServiceMapper.deliveryChart365Day(dateTimeParm365);
+                deliveryChartResults = orderStatisticsServiceMapper.eDDeliveryChart365Day(dateTimeParm365);
+                break;
+
+            default:
+                throw new SesWebDeliveryException(ExceptionCodeEnums.OPERATION_ERROR.getCode(), ExceptionCodeEnums.OPERATION_ERROR.getMessage());
+
+        }
+
+        List<String> dateList = new LinkedList();
+        dateList = getDateList(heavens, enter.getDateTimes());
+
+        if (deliveryChartResults.size() > 0) {
+
+            //获取最大值
+            max = deliveryChartResults.stream().mapToDouble(DeliveryChartResult::getTotal).max().getAsDouble();
+            //获取平均值
+            avg = deliveryChartResults.stream().mapToDouble(DeliveryChartResult::getTotal).average().getAsDouble();
+            //取最小值
+            min = deliveryChartResults.stream().mapToDouble(DeliveryChartResult::getTotal).min().getAsDouble();
+
+            DeliveryChartResult result = null;
+
+            for (String str : dateList) {
+                for (DeliveryChartResult chart : deliveryChartResults) {
+                    if (chart.getTimes().equals(str)) {
+                        map.put(str, chart);
+                    }
+                }
+                if (!map.containsKey(str)) {
+                    result = new DeliveryChartResult();
+                    result.setTimes(str);
+                    map.put(str, result);
+                }
+            }
+        }else{
+            map=null;
+        }
+
+        DeliveryChartListResult result = new DeliveryChartListResult();
+        result.setMap(map);
+        result.setAvg(avg);
+        result.setMax(max);
+        result.setMin(min);
+
+        return result;
+    }
+
+    private List<String> getDateList(int heavens, Date date) {
+        ArrayList<String> list = new ArrayList<>();
+        switch (heavens) {
+            case 1:
+                list = DateUtil.get24HourList(DateUtil.getDateTimeStamp(date));
+                break;
+            case 7:
+                list = DateUtil.getDayList(date, 7, null);
+                break;
+            case 30:
+                list = DateUtil.getDayList(date, 30, null);
+                break;
+            case 365:
+                list = DateUtil.getDayList(date, 365, DateUtil.DEFAULT_YYMM_FORMAT);
+                break;
+        }
+
+        return checkDayResultSingle(list);
+    }
+
+    //去除重复的时间，只供柱状图使用。
+    private ArrayList<String> checkDayResultSingle(ArrayList<String> dayList) {
+        ArrayList<String> temp = new ArrayList<String>();
+        Iterator<String> iterator = dayList.iterator();
+        while (iterator.hasNext()) {
+            String str = iterator.next();
+            if (!temp.contains(str)) {
+                temp.add(str);
+            }
+        }
+        return temp;
     }
 }
