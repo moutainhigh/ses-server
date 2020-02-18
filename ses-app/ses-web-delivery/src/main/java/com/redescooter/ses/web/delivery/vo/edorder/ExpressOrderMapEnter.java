5828521a483d9b08d8c86f7011de67a71c5e2706
李加廷 <lijiating621@gmail.com>
2020-02-18 17:56:43 +0800

    快递订单地图订单筛选修改bug

--- ses-app/ses-web-delivery/src/main/java/com/redescooter/ses/web/delivery/vo/edorder/ExpressOrderMapEnter.java
+++ ses-app/ses-web-delivery/src/main/java/com/redescooter/ses/web/delivery/vo/edorder/ExpressOrderMapEnter.java
@@ -3,6 +3,7 @@ package com.redescooter.ses.web.delivery.vo.edorder;
 import com.redescooter.ses.api.common.vo.base.GeneralEnter;
 import lombok.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import io.swagger.annotations.*;
@@ -22,5 +23,5 @@ import io.swagger.annotations.*;
 public class ExpressOrderMapEnter extends GeneralEnter {
 
     @ApiModelProperty(value = "状态列表")
-    private List<String> statusList;
+    private List<String> statusList= new ArrayList<>();
 }
