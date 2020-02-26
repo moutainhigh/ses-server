0f65f7a87eba5ad1135c044cb8f68c54546a4950
alex <alex@redescooter.com>
2020-02-26 16:19:34 +0800

    生成基本文件

---
+++ ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/dao/BomRosServiceMapper.java
@@ -0,0 +1,11 @@
+package com.redescooter.ses.web.ros.dao;
+
+/**
+ * @ClassName:BomRosServiceMapper
+ * @description: BomRosServiceMapper
+ * @author: Alex
+ * @Version：1.3
+ * @create: 2020/02/26 16:18
+ */
+public interface BomRosServiceMapper {
+}
