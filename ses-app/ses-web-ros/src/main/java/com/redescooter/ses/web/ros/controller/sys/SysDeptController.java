a391924099c0a4281622c59a6656e8787136b25e
李加廷 <lijiating621@gmail.com>
2020-03-10 15:05:58 +0800

    :sparkles:update

---
+++ ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/controller/sys/SysDeptController.java
@@ -0,0 +1,19 @@
+package com.redescooter.ses.web.ros.controller.sys;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName SysDeptController
+ * @Author Jerry
+ * @date 2020/03/10 14:59
+ * @Description:
+ */
+@Api(tags = {"部门管理"})
+@CrossOrigin
+@RestController
+@RequestMapping(value = "/sys/dept")
+public class SysDeptController {
+}
