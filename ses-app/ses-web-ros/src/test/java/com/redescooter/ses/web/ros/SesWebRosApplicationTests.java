9a70732d6f7f5a8bc3e04a85024bbabde59121ab
李加廷 <lijiating621@gmail.com>
2020-03-20 15:18:06 +0800

    :speech_balloon: 设置sysAdmin最高权限

--- ses-app/ses-web-ros/src/test/java/com/redescooter/ses/web/ros/SesWebRosApplicationTests.java
+++ ses-app/ses-web-ros/src/test/java/com/redescooter/ses/web/ros/SesWebRosApplicationTests.java
@@ -1,8 +1,23 @@
 package com.redescooter.ses.web.ros;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SesWebRosApplicationTests {
+
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("单元测试开始--------------------->");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("单元测试结束--------------------->");
+    }
 
 }
