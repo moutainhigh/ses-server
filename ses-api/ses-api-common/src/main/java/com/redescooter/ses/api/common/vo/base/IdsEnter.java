e64958f901c9f9f78da9af9ffa4694dd7ba200d2
alex <alex@redescooter.com>
2020-04-08 15:27:55 +0800

    移动idsEnter 对象

--- ses-api/ses-api-common/src/main/java/com/redescooter/ses/api/common/vo/base/IdsEnter.java
+++ ses-api/ses-api-common/src/main/java/com/redescooter/ses/api/common/vo/base/IdsEnter.java
@@ -1,9 +1,12 @@
-package com.redescooter.ses.api.common.enums.base;
+package com.redescooter.ses.api.common.vo.base;
 
-import com.redescooter.ses.api.common.vo.base.GeneralEnter;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 @ApiModel(value = "列表主键", description = "列表主键")
