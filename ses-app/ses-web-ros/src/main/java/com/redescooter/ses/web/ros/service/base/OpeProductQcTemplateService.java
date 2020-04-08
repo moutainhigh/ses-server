29ca7fa98f540f69e6cd3e312426fa15008ad161
alex <alex@redescooter.com>
2020-04-08 15:14:44 +0800

    产品质检模板、部件质检模板开发完成

--- ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/service/base/OpeProductQcTemplateService.java
+++ ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/service/base/OpeProductQcTemplateService.java
@@ -2,9 +2,12 @@ package com.redescooter.ses.web.ros.service.base;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.redescooter.ses.web.ros.dm.OpeProductQcTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-public interface OpeProductQcTemplateService extends IService<OpeProductQcTemplate>{
+
+@Transactional
+public interface OpeProductQcTemplateService extends IService<OpeProductQcTemplate> {
 
 
     int updateBatch(List<OpeProductQcTemplate> list);
