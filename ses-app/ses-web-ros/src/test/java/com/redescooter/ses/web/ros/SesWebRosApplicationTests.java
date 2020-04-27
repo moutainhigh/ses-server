d746835ac7f07f048ad8254c1dfddd960f94bdbf
alex <alex@redescooter.com>
2020-04-27 10:43:52 +0800

    alex： 将production分支 合并到RPS 分支上

--- ses-app/ses-web-ros/src/test/java/com/redescooter/ses/web/ros/SesWebRosApplicationTests.java
+++ ses-app/ses-web-ros/src/test/java/com/redescooter/ses/web/ros/SesWebRosApplicationTests.java
@@ -5,7 +5,9 @@ import com.redescooter.ses.starter.redis.service.JedisService;
 import lombok.extern.log4j.Log4j;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
