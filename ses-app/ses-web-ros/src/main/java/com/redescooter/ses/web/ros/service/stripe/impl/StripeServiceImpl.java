85c873c356bcd36e31b47f4a259f02e78573b656
jerry.li <mrjerryli@yeah.net>
2020-05-20 03:35:48 +0800

    paymentIntent添加订单信息，用于钩子进行回调

--- ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/service/stripe/impl/StripeServiceImpl.java
+++ ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/service/stripe/impl/StripeServiceImpl.java
@@ -3,10 +3,10 @@ package com.redescooter.ses.web.ros.service.stripe.impl;
 import com.redescooter.ses.api.common.vo.base.IdEnter;
 import com.redescooter.ses.api.common.vo.base.StringResult;
 import com.redescooter.ses.web.ros.dm.OpeCustomerInquiry;
+import com.redescooter.ses.web.ros.exception.ExceptionCodeEnums;
+import com.redescooter.ses.web.ros.exception.SesWebRosException;
 import com.redescooter.ses.web.ros.service.base.OpeCustomerInquiryService;
 import com.redescooter.ses.web.ros.service.stripe.StripeService;
-import com.redescooter.ses.web.ros.service.website.WebsiteOrderFormService;
-import com.redescooter.ses.web.ros.vo.website.OrderFormInfoResult;
 import com.stripe.Stripe;
 import com.stripe.model.PaymentIntent;
 import com.stripe.param.PaymentIntentCreateParams;
@@ -16,12 +16,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Slf4j
 @Service
 public class StripeServiceImpl implements StripeService {
 
-    @Value("${stripe.test.secret_key}")
+    private final String integrationCheck = "integration_check";
+
+    @Value("${stripe.secret_key}")
     private String API_SECRET_KEY;
+
+    @Value("${stripe.receipt_email}")
+    private String ReceiptEmail;
+
+    @Value("${stripe.currency}")
+    private String Currency;
+
+    @Value("${stripe.payment_method_types}")
+    private String PaymentMethodType;
+
+    @Value("${stripe.payment_event}")
+    private String PaymentEvent;
 
     @Autowired
     private OpeCustomerInquiryService opeCustomerInquiryService;
@@ -33,19 +50,32 @@ public class StripeServiceImpl implements StripeService {
         StringResult result = new StringResult();
 
         Stripe.apiKey = API_SECRET_KEY;
-        log.info("测试环境证书为===={}",API_SECRET_KEY);
+        log.info("====================================");
+        log.info("API_SECRET_KEY===={}", API_SECRET_KEY);
+        log.info("ReceiptEmail===={}", ReceiptEmail);
+        log.info("Currency===={}", Currency);
+        log.info("PaymentMethodType===={}", PaymentMethodType);
+        log.info("PaymentEvent===={}", PaymentEvent);
+        log.info("====================================");
 
         OpeCustomerInquiry payOrder = opeCustomerInquiryService.getById(enter.getId());
 
-        if(payOrder==null){
-            return result;
+        if (payOrder == null) {
+            throw new SesWebRosException(ExceptionCodeEnums.PAYMENT_INFO_IS_NOT_EXIST.getCode(), ExceptionCodeEnums.PAYMENT_INFO_IS_NOT_EXIST.getMessage());
         }
+
+        Map<String, String> map = new HashMap<>();
+        map.put(integrationCheck, PaymentEvent);
+        map.put("order_id", String.valueOf(payOrder.getId()));
+        map.put("order_no", payOrder.getOrderNo());
 
         PaymentIntentCreateParams params =
                 PaymentIntentCreateParams.builder()
-                        .setCurrency("eur")
+                        .setReceiptEmail(ReceiptEmail)
+                        .setCurrency(Currency)
+                        .addPaymentMethodType(PaymentMethodType)
                         .setAmount(payOrder.getTotalPrice().longValue())
-                        .putMetadata("integration_check", "accept_a_payment")
+                        .putAllMetadata(map)
                         .build();
 
         PaymentIntent intent = PaymentIntent.create(params);
--- ses-app/ses-web-ros/src/test/java/com/redescooter/ses/web/ros/service/stripe/impl/StripeServiceImplTest.java
+++ ses-app/ses-web-ros/src/test/java/com/redescooter/ses/web/ros/service/stripe/impl/StripeServiceImplTest.java
@@ -4,22 +4,63 @@ import com.redescooter.ses.api.common.vo.base.IdEnter;
 import com.redescooter.ses.api.common.vo.base.StringResult;
 import com.redescooter.ses.web.ros.SesWebRosApplicationTests;
 import com.redescooter.ses.web.ros.service.stripe.StripeService;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentMethod;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Slf4j
 public class StripeServiceImplTest extends SesWebRosApplicationTests {
 
+    @Value("${stripe.secret_key}")
+    private String API_SECRET_KEY;
+
     @Autowired
     private StripeService stripeService;
 
+    /**
+     * 支付授权单元测试
+     */
     @Test
     public void testPaymentIntent() {
 
         StringResult result = stripeService.paymentIntent(new IdEnter(1004221l));
-        log.info(result.getValue());
 
+        log.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        log.info("result===={}", result.getValue());
+        log.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+
+    }
+
+    @Test
+    public void testPaymentMethod() {
+
+        Stripe.apiKey = API_SECRET_KEY;
+
+        Map<String, Object> card = new HashMap<>();
+        card.put("number", "4242424242424242");
+        card.put("exp_month", 5);
+        card.put("exp_year", 2021);
+        card.put("cvc", "314");
+        Map<String, Object> params = new HashMap<>();
+        params.put("type", "card");
+        params.put("card", card);
+
+        try {
+            PaymentMethod paymentMethod =
+                    PaymentMethod.create(params);
+
+            log.info(paymentMethod.getId());
+        } catch (StripeException e) {
+            e.printStackTrace();
+        }
     }
 }
