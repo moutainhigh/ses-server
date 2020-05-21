194e4334ba1f222e8c2ffc1f31a1075404f678a6
jerry.li <mrjerryli@yeah.net>
2020-05-22 04:09:04 +0800

    run is ok  testing

--- ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/service/website/impl/WebsiteTokenServiceImpl.java
+++ ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/service/website/impl/WebsiteTokenServiceImpl.java
@@ -172,7 +172,9 @@ public class WebsiteTokenServiceImpl implements WebSiteTokenService {
   @Override
   public UserToken checkCustomerToken(GeneralEnter enter) {
     UserToken userToken = getUserToken(enter.getToken());
-    if (!StringUtils.equals(userToken.getClientType(), enter.getClientType()) || !StringUtils.equals(userToken.getSystemId(), enter.getSystemId()) || !StringUtils.equals(userToken.getAppId(),
+    if (!StringUtils.equals(userToken.getClientType(), enter.getClientType()) ||
+            !StringUtils.equals(userToken.getSystemId(), enter.getSystemId()) ||
+            !StringUtils.equals(userToken.getAppId(),
       enter.getAppId())) {
       throw new SesWebRosException(ExceptionCodeEnums.TOKEN_NOT_EXIST.getCode(), ExceptionCodeEnums.TOKEN_NOT_EXIST.getMessage());
     }
