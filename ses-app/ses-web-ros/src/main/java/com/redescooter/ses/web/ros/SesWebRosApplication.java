c8fd7ad494836aa7e35e790834a368156fa5d203
aleks-xie <aleks@redescooter.com>
2020-06-02 15:30:01 +0800

    微调

--- ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/SesWebRosApplication.java
+++ ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/SesWebRosApplication.java
@@ -1,15 +1,11 @@
 package com.redescooter.ses.web.ros;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
-import com.ulisesbocchio.jasyptspringboot.environment.StandardEncryptableEnvironment;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubboConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.PostConstruct;
@@ -21,7 +17,6 @@ import java.util.TimeZone;
 @EnableAutoConfiguration
 @SpringBootApplication(scanBasePackages = {"com.redescooter.ses"})
 @EnableTransactionManagement
-//@PropertySource(value = "classpath:application-local.yml")
 public class SesWebRosApplication {
 
     private static volatile boolean running = true;
