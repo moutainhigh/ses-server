5a863534c345a1c5bea2fc6dd8f9c110b2cefe87
mrjerryli <mrjerryli@yeah.net>
2020-07-30 04:09:16 +0800

    feat(tool): 去除重复jar
    去除start-tool重复包

--- ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/service/monday/impl/MondayServiceImpl.java
+++ ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/service/monday/impl/MondayServiceImpl.java
@@ -43,7 +43,6 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
