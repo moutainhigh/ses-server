21b325007ffea12d009b268f76017b5a121a95e7
alex@redescooter.com <alex@redescooter.com>
2020-08-14 16:00:34 +0800

    fix：暂存

--- ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/service/customer/impl/InquiryServiceImpl.java
+++ ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/service/customer/impl/InquiryServiceImpl.java
@@ -7,7 +7,6 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.google.common.base.Strings;
 import com.redescooter.ses.api.common.enums.base.AppIDEnums;
-import com.redescooter.ses.api.common.enums.base.SystemIDEnums;
 import com.redescooter.ses.api.common.enums.customer.CustomerAccountFlagEnum;
 import com.redescooter.ses.api.common.enums.customer.CustomerSourceEnum;
 import com.redescooter.ses.api.common.enums.customer.CustomerStatusEnum;
@@ -29,17 +28,10 @@ import com.redescooter.ses.web.ros.constant.SequenceName;
 import com.redescooter.ses.web.ros.dao.InquiryServiceMapper;
 import com.redescooter.ses.web.ros.dm.OpeCustomer;
 import com.redescooter.ses.web.ros.dm.OpeCustomerInquiry;
-import com.redescooter.ses.web.ros.dm.OpeDistrustLead;
-import com.redescooter.ses.web.ros.dm.OpeSysUserProfile;
-import com.redescooter.ses.web.ros.dm.OpeDistrustLead;
 import com.redescooter.ses.web.ros.exception.ExceptionCodeEnums;
 import com.redescooter.ses.web.ros.exception.SesWebRosException;
 import com.redescooter.ses.web.ros.service.base.OpeCustomerInquiryService;
 import com.redescooter.ses.web.ros.service.base.OpeCustomerService;
-import com.redescooter.ses.web.ros.service.base.OpeDistrustLeadService;
-import com.redescooter.ses.web.ros.service.base.OpeSysUserProfileService;
-import com.redescooter.ses.web.ros.service.base.OpeDistrustLeadService;
-import com.redescooter.ses.web.ros.service.base.OpeSysUserProfileService;
 import com.redescooter.ses.web.ros.service.customer.InquiryService;
 import com.redescooter.ses.web.ros.service.excel.ExcelService;
 import com.redescooter.ses.web.ros.service.monday.MondayService;
@@ -64,7 +56,6 @@ import redis.clients.jedis.JedisCluster;
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * @ClassName:InquiryServiceImpl
@@ -76,50 +67,44 @@ import java.util.stream.Collectors;
 @Slf4j
 @Service
 public class InquiryServiceImpl implements InquiryService {
-    
-    
-    @Autowired
-    private OpeCustomerInquiryService opeCustomerInquiryService;
-    
-    @Autowired
-    private JedisCluster jedisCluster;
-    
-    @Autowired
-    private InquiryServiceMapper inquiryServiceMapper;
-    
-    @Autowired
-    private OpeDistrustLeadService opeDistrustLeadService;
+
 
     @Autowired
-    private OpeSysUserProfileService opeSysUserProfileService;
+    private OpeCustomerInquiryService opeCustomerInquiryService;
+
+    @Autowired
+    private JedisCluster jedisCluster;
+
+    @Autowired
+    private InquiryServiceMapper inquiryServiceMapper;
 
     @Autowired
     private OpeCustomerService opeCustomerService;
-    
+
     @Reference
     private MailMultiTaskService mailMultiTaskService;
-    
+
     @Reference
     private CityBaseService cityBaseService;
-    
+
     @Reference
     private IdAppService idAppService;
-    
+
     @Autowired
     private ExcelService excelService;
-    
+
     @Value("${Request.privateKey}")
     private String privateKey;
-    
+
     @Value("${excel.folder}")
     private String excelFolder;
-    
+
     @Autowired
     private MondayService mondayService;
-    
+
     @Autowired
     private OssConfig ossConfig;
-    
+
     @Autowired
     private ContactUsService contactUsService;
 
@@ -130,7 +115,7 @@ public class InquiryServiceImpl implements InquiryService {
         statusResultList.forEach(item -> {
             map.put(item.getStatus(), item.getTotalCount());
         });
-        
+
         for (InquiryStatusEnums item : InquiryStatusEnums.values()) {
             if (!map.containsKey(item.getValue())) {
                 map.put(item.getValue(), 0);
@@ -140,7 +125,7 @@ public class InquiryServiceImpl implements InquiryService {
         map.remove(InquiryStatusEnums.PAY_LAST_PARAGRAPH.getValue());
         return map;
     }
-    
+
     /**
      * @param saveInquiryEnter
      * @desc: 保存询价单
@@ -174,10 +159,10 @@ public class InquiryServiceImpl implements InquiryService {
             //邮箱 去空格
             enter.setEmail(SesStringUtils.stringTrim(enter.getEmail()));
         }
-        
+
         // 查询已存在的email 暂时注释掉 邮箱过滤
         //List<String> emailList = inquiryServiceMapper.usingEmailList();
-        
+
         //是否允许正式客户在联系我们中留言 默认为false  前端控制
         if (!enter.getWhetherConstantUs()) {
             //查询 该邮箱 是否为正式客户 是的话 直接返回
@@ -187,18 +172,6 @@ public class InquiryServiceImpl implements InquiryService {
             if (opeCustomer != null) {
                 return new GeneralResult(enter.getRequestId());
             }
-        }
-        //查询客户是否有询价单 存在的话数量累计
-        List<OpeCustomerInquiry> customerInquiryList =
-                opeCustomerInquiryService.list(new LambdaQueryWrapper<OpeCustomerInquiry>().eq(OpeCustomerInquiry::getEmail, enter.getEmail()).ne(OpeCustomerInquiry::getStatus,
-                        InquiryStatusEnums.DECLINE.getValue()));
-        
-        OpeCustomerInquiry opeCustomerInquiry = null;
-        if (CollectionUtils.isEmpty(customerInquiryList)) {
-            opeCustomerInquiry = buildOpeCustomerInquiry(enter);
-        } else {
-            opeCustomerInquiry = customerInquiryList.get(0);
-            opeCustomerInquiry.setScooterQuantity(opeCustomerInquiry.getScooterQuantity() + 1);
         }
 
 //        //查询客户是否有询价单 存在的话数量累计
@@ -220,47 +193,17 @@ public class InquiryServiceImpl implements InquiryService {
 //        }*/
 //        opeCustomerInquiry.setSource("1");
 //        opeCustomerInquiryService.saveOrUpdate(opeCustomerInquiry);
-        opeCustomerInquiry.setSource("1");
-        opeCustomerInquiryService.saveOrUpdate(opeCustomerInquiry);
-        try {
-            //通知区域负责人
-            sendDistrustleadEmail(enter);
-        } catch (Exception e) {
-        
-        }
+//
+        // 官网联系我们
+        contactUsService.websiteContactUs(enter);
+    
         //Monday 同步数据
-        mondayService.websiteContantUs(opeCustomerInquiry);
+//        mondayService.websiteContantUs(opeCustomerInquiry);
         return new GeneralResult(enter.getRequestId());
     }
-    
-    //发送区域负责人
-    public void sendDistrustleadEmail(SaveInquiryEnter enter) {
-        BaseMailTaskEnter mailTask = new BaseMailTaskEnter();
-//      mailTask.setEvent(MailTemplateEventEnums.ROS_DISTRUST_LEAD_SEND_MAIL.getEvent());
-        mailTask.setSystemId(SystemIDEnums.REDE_SES.getSystemId());
-        mailTask.setAppId(AppIDEnums.SES_ROS.getValue());
-        mailTask.setRequestId(enter.getRequestId());
-        mailTask.setUserId(enter.getUserId());
-        mailTask.setCountry(enter.getCountry());
-        mailTask.setTimeZone(enter.getTimeZone());
-        //查询区域负责人信息
-        List<OpeDistrustLead> list = opeDistrustLeadService.list(new QueryWrapper<OpeDistrustLead>().eq(OpeDistrustLead.COL_DISTRUST_NAME, enter.getTimeZone()).eq(OpeDistrustLead.COL_CITY_NAME,
-                enter.getTimeZone()));
-        
-        if (list != null && !list.isEmpty()) {
-            list.forEach(item -> {
-                //查询负责人完整信息
-                OpeSysUserProfile one = opeSysUserProfileService.getOne(new QueryWrapper<OpeSysUserProfile>().eq(OpeSysUserProfile.COL_ID, item.getUserId()));
-                mailTask.setName(one.getFullName());
-                mailTask.setEmail(one.getEmail());
-                mailMultiTaskService.sendForgetPasswordEmaillTask(mailTask);
-            });
-        }
-        
-    }
-    
+
     private OpeCustomerInquiry buildOpeCustomerInquiry(SaveInquiryEnter enter) {
-        
+
         OpeCustomerInquiry opeCustomerInquiry = new OpeCustomerInquiry();
         opeCustomerInquiry.setId(idAppService.getId(SequenceName.OPE_CUSTOMER_INQUIRY));
         opeCustomerInquiry.setDr(0);
@@ -272,11 +215,11 @@ public class InquiryServiceImpl implements InquiryService {
         opeCustomerInquiry.setSalesId(0L);
         opeCustomerInquiry.setSource(InquirySourceEnums.INQUIRY.getValue());
         opeCustomerInquiry.setStatus(InquiryStatusEnums.UNPROCESSED.getValue());
-        
+
         //默认为个人餐厅
 //        opeCustomerInquiry.setIndustry(CustomerIndustryEnums.RESTAURANT.getValue());
 //        opeCustomerInquiry.setCustomerType(CustomerTypeEnum.PERSONAL.getValue());
-        
+
         opeCustomerInquiry.setCompanyName(null);
         opeCustomerInquiry.setFirstName(SesStringUtils.upperCaseString(enter.getFirstName()));
         opeCustomerInquiry.setLastName(SesStringUtils.upperCaseString(enter.getLastName()));
@@ -294,7 +237,7 @@ public class InquiryServiceImpl implements InquiryService {
         opeCustomerInquiry.setUpdatedBy(0L);
         opeCustomerInquiry.setCreatedTime(new Date());
         opeCustomerInquiry.setUpdatedTime(new Date());
-        
+
         //def1 国家 def2 城市 distrust区域
         opeCustomerInquiry.setDef1(enter.getCustomerCountry());
         opeCustomerInquiry.setDistrict(Long.valueOf(enter.getDistrust()));
@@ -303,7 +246,7 @@ public class InquiryServiceImpl implements InquiryService {
         opeCustomerInquiry.setCountry(enter.getCountryId());
         return opeCustomerInquiry;
     }
-    
+
     /**
      * @param enter
      * @desc: 询价单列表
@@ -337,7 +280,7 @@ public class InquiryServiceImpl implements InquiryService {
 //        });
         return PageResult.create(enter, count, inquiryResultList);
     }
-    
+
     /**
      * @param enter
      * @desc: 询价单详情
@@ -364,7 +307,7 @@ public class InquiryServiceImpl implements InquiryService {
 //        }
 //        inquiryResult.setCityName(city);
 //        inquiryResult.setDistrustName(distrust);
-        
+
         if (StringUtils.equals(inquiryResult.getStatus(), InquiryStatusEnums.UNPAY_DEPOSIT.getValue()) || StringUtils.equals(inquiryResult.getStatus(), InquiryStatusEnums.PAY_DEPOSIT.getValue())) {
             //验证是否可以再次发生邮件
             Boolean exists = jedisCluster.exists(new StringBuffer(inquiryResult.getId().toString()).append("send::").append(inquiryResult.getEmail()).toString());
@@ -377,7 +320,7 @@ public class InquiryServiceImpl implements InquiryService {
         }
         return inquiryResult;
     }
-    
+
     /**
      * 定金支付邮件
      *
@@ -397,7 +340,7 @@ public class InquiryServiceImpl implements InquiryService {
         if (!StringUtils.equals(opeCustomerInquiry.getStatus(), InquiryStatusEnums.UNPAY_DEPOSIT.getValue())) {
             throw new SesWebRosException(ExceptionCodeEnums.STATUS_ILLEGAL.getCode(), ExceptionCodeEnums.STATUS_ILLEGAL.getMessage());
         }
-        
+
         // 创建邮件任务
         BaseMailTaskEnter baseMailTaskEnter = new BaseMailTaskEnter();
         baseMailTaskEnter.setName(opeCustomerInquiry.getFullName());
@@ -410,14 +353,14 @@ public class InquiryServiceImpl implements InquiryService {
         baseMailTaskEnter.setMailAppId(AppIDEnums.SAAS_APP.getValue());
         baseMailTaskEnter.setMailSystemId(AppIDEnums.SAAS_APP.getSystemId());
         mailMultiTaskService.addCustomerInquiryPayDepositTask(baseMailTaskEnter);
-        
+
         //设置邮箱发送有效时间
         String key = new StringBuffer(opeCustomerInquiry.getId().toString()).append("send::").append(opeCustomerInquiry.getEmail()).toString();
         jedisCluster.set(key, DateUtil.getDate());
         jedisCluster.expire(key, new Long(RedisExpireEnum.MINUTES_3.getSeconds()).intValue());
         return new GeneralResult(enter.getRequestId());
     }
-    
+
     /**
      * 尾款支付邮件
      *
@@ -437,7 +380,7 @@ public class InquiryServiceImpl implements InquiryService {
         if (!StringUtils.equals(opeCustomerInquiry.getStatus(), InquiryStatusEnums.PAY_DEPOSIT.getValue())) {
             throw new SesWebRosException(ExceptionCodeEnums.STATUS_ILLEGAL.getCode(), ExceptionCodeEnums.STATUS_ILLEGAL.getMessage());
         }
-        
+
         // 创建邮件任务
         BaseMailTaskEnter baseMailTaskEnter = new BaseMailTaskEnter();
         baseMailTaskEnter.setName(opeCustomerInquiry.getFullName());
@@ -450,14 +393,14 @@ public class InquiryServiceImpl implements InquiryService {
         baseMailTaskEnter.setMailAppId(AppIDEnums.SES_ROS.getValue());
         baseMailTaskEnter.setMailSystemId(AppIDEnums.SES_ROS.getSystemId());
         mailMultiTaskService.addCustomerInquiryPayLastParagraphTask(baseMailTaskEnter);
-        
+
         //设置邮箱发送有效时间
         String key = new StringBuffer(opeCustomerInquiry.getId().toString()).append("send::").append(opeCustomerInquiry.getEmail()).toString();
         jedisCluster.set(key, DateUtil.getDate());
         jedisCluster.expire(key, new Long(RedisExpireEnum.MINUTES_3.getSeconds()).intValue());
         return new GeneralResult(enter.getRequestId());
     }
-    
+
     /**
      * 接受询价单 转化为潜在客户
      *
@@ -484,13 +427,13 @@ public class InquiryServiceImpl implements InquiryService {
         opeCustomerInquiry.setUpdatedBy(enter.getUserId());
         opeCustomerInquiry.setUpdatedTime(new Date());
         opeCustomerInquiryService.updateById(opeCustomerInquiry);
-        
+
         // 保存客户信息
         OpeCustomer opeCustomer = buildOpeCustomerSingle(enter, opeCustomerInquiry);
         opeCustomerService.insertOrUpdateSelective(opeCustomer);
         return new GeneralResult(enter.getRequestId());
     }
-    
+
     /**
      * 拒绝询价单
      *
@@ -517,7 +460,7 @@ public class InquiryServiceImpl implements InquiryService {
         opeCustomerInquiryService.updateById(opeCustomerInquiry);
         return new GeneralResult(enter.getRequestId());
     }
-    
+
     /**
      * 询价单数据导出
      *
@@ -544,7 +487,7 @@ public class InquiryServiceImpl implements InquiryService {
                 FileInputStream inputStream = new FileInputStream(file);
                 MultipartFile multipartFile = new MockMultipartFile(file.getName(), file.getName(),
                         ContentType.APPLICATION_OCTET_STREAM.toString(), inputStream);
-                
+
                 ClientConfiguration conf = new ClientConfiguration();
                 conf.setProtocol(ProtocolEnums.getProtocol(ossConfig.getProtocol()));
                 OSSClient ossClient = null;
@@ -559,13 +502,13 @@ public class InquiryServiceImpl implements InquiryService {
                     file.delete();
                 }
             } catch (Exception e) {
-            
+
             }
         }
         return new GeneralResult(excelPath);
     }
-    
-    
+
+
     private Map<String, Object> toMap(InquiryResult opeCustomerInquiry) {
         Map<String, Object> map = new LinkedHashMap<>();
         map.put("NAME", Strings.isNullOrEmpty(opeCustomerInquiry.getCustomerFirstName()) ? "--" : opeCustomerInquiry.getCustomerFirstName());
@@ -578,8 +521,8 @@ public class InquiryServiceImpl implements InquiryService {
         map.put("CREATE TIME", opeCustomerInquiry.getAcceptanceTime() == null ? "--" : DateUtil.getTimeStrDefault(opeCustomerInquiry.getAcceptanceTime()));
         return map;
     }
-    
-    
+
+
     private OpeCustomer buildOpeCustomerSingle(IdEnter enter, OpeCustomerInquiry opeCustomerInquiry) {
         OpeCustomer opeCustomer = new OpeCustomer();
         opeCustomer.setId(idAppService.getId(SequenceName.OPE_CUSTOMER));
@@ -637,5 +580,5 @@ public class InquiryServiceImpl implements InquiryService {
         opeCustomer.setUpdatedTime(new Date());
         return opeCustomer;
     }
-    
+
 }
