582faf9c7db9b53a8cdc120ada2313c2a2632027
joan <joan@redescooter.com>
2020-09-02 17:31:42 +0800

    feat：编写部门模块

---
+++ ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/dao/sys/PositionServiceMapper.java
@@ -0,0 +1,12 @@
+package com.redescooter.ses.web.ros.dao.sys;
+
+/**
+ * @ClassNameSysPositionMapper
+ * @Description
+ * @Author Joan
+ * @Date2020/9/2 17:28
+ * @Version V1.0
+ **/
+public interface PositionServiceMapper{
+
+}
