06661df5095002b52a7ce422bb483997acfd5f81
joan <joan@redescooter.com>
2020-09-03 13:54:03 +0800

    feat：增加职位类型接口

--- ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/service/sys/SysPositionService.java
+++ ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/service/sys/SysPositionService.java
@@ -2,6 +2,7 @@ package com.redescooter.ses.web.ros.service.sys;
 
 import com.redescooter.ses.api.common.vo.base.GeneralEnter;
 import com.redescooter.ses.web.ros.vo.sys.position.PositionTypeResult;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -12,6 +13,7 @@ import java.util.List;
  * @Date2020/9/2 17:25
  * @Version V1.0
  **/
+@Service
 public interface SysPositionService {
     /**
      * 岗位类型查询
