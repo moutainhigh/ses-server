a7fe491638fc7c983021f3d22363a0d04f229218
alex <alex@redescooter.com>
2020-09-03 14:08:37 +0800

    fix：Monday模版加载添加开关

--- ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/config/MondayConfig.java
+++ ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/config/MondayConfig.java
@@ -1,7 +1,6 @@
 package com.redescooter.ses.web.ros.config;
 
 import lombok.Data;
-import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -54,4 +53,7 @@ public class MondayConfig {
 
     //订阅邮件分组
     private String subEmailGroupName;
+
+    //是否加载模版
+    private Boolean loadTemplate;
 }
