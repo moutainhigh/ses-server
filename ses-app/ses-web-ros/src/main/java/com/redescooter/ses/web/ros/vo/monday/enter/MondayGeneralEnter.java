754491cded8f5205c299d89d8e2bdefb7bbd8579
aleks-xie <aleks@redescooter.com>
2020-09-16 19:15:16 +0800

    style:微调

--- ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/vo/monday/enter/MondayGeneralEnter.java
+++ ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/vo/monday/enter/MondayGeneralEnter.java
@@ -1,6 +1,8 @@
 package com.redescooter.ses.web.ros.vo.monday.enter;
 
 import com.redescooter.ses.api.common.vo.base.GeneralEnter;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
