1b5e71e511b809ce9aef212217db035b64d84e87
alex <alex@redescooter.com>
2020-09-28 18:06:46 +0800

    feat：添加删除接口

---
+++ ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/service/admin/DeleteRosLoginDateService.java
@@ -0,0 +1,12 @@
+package com.redescooter.ses.web.ros.service.admin;
+
+import com.redescooter.ses.api.common.vo.base.StringEnter;
+
+public interface DeleteRosLoginDateService {
+    /**
+     * 删除和当前 邮箱相关的所有数据 包含：sysUser、customer、inquiry
+     * 
+     * @param enter
+     */
+    public void deleteRosLoginDate(StringEnter enter);
+}
