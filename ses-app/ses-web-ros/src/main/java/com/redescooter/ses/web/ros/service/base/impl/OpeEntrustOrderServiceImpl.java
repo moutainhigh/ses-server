447baee531e31d215229c7252c34903d1d3ed22f
aleks-xie <aleks@redescooter.com>
2020-11-03 19:47:16 +0800

    feat:委托单表代码重新生成

--- ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/service/base/impl/OpeEntrustOrderServiceImpl.java
+++ ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/service/base/impl/OpeEntrustOrderServiceImpl.java
@@ -1,17 +1,12 @@
 package com.redescooter.ses.web.ros.service.base.impl;
 
-import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
-import java.util.List;
-
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.redescooter.ses.web.ros.dao.base.OpeEntrustOrderMapper;
-
-import java.util.List;
-
 import com.redescooter.ses.web.ros.dm.OpeEntrustOrder;
 import com.redescooter.ses.web.ros.service.base.OpeEntrustOrderService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OpeEntrustOrderServiceImpl extends ServiceImpl<OpeEntrustOrderMapper, OpeEntrustOrder> implements OpeEntrustOrderService {
