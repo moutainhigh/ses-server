9760cefe29629b5e9c06e47507a703565ed3bdca
assert <assert@redescooter.com>
2020-12-29 15:59:11 +0800

    fix: [OMS-应用版本管理-ECU发布版本] 完善ECU发布版本失败定时重试逻辑

--- ses-api/ses-api-foundation/src/main/java/com/redescooter/ses/api/foundation/vo/app/AppVersionUpdateLogDetailDTO.java
+++ ses-api/ses-api-foundation/src/main/java/com/redescooter/ses/api/foundation/vo/app/AppVersionUpdateLogDetailDTO.java
@@ -2,6 +2,7 @@ package com.redescooter.ses.api.foundation.vo.app;
 
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.Date;
 
 /**
@@ -9,7 +10,7 @@ import java.util.Date;
  * @date 2020/12/27 17:12
  */
 @Data
-public class AppVersionUpdateLogDetailDTO {
+public class AppVersionUpdateLogDetailDTO implements Serializable {
 
     /**
      * 主键id
