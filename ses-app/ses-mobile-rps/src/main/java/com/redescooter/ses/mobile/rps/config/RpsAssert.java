891e7858a36b8fd2566daa642aa4aadc884a756a
assert <assert@redescooter.com>
2021-01-07 11:02:38 +0800

    feat: [ses-mobile-rps] 添加rps项目断言工具类

---
+++ ses-app/ses-mobile-rps/src/main/java/com/redescooter/ses/mobile/rps/config/RpsAssert.java
@@ -0,0 +1,63 @@
+package com.redescooter.ses.mobile.rps.config;
+
+import com.redescooter.ses.mobile.rps.exception.SesMobileRpsException;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Collection;
+
+/**
+ * RPS项目断言工具类
+ * @author assert
+ * @date 2021/1/7 10:53
+ */
+public class RpsAssert {
+
+    /**
+     * obj == null throw SesMobileRpsException
+     * @param obj
+     * @param code
+     * @param msg
+     */
+    public static void isNull(Object obj, Integer code, String msg) {
+        if (obj == null) {
+            throw new SesMobileRpsException(code, msg);
+        }
+    }
+
+    /**
+     * obj is blank throw SesMobileRpsException
+     * @param obj
+     * @param code
+     * @param msg
+     */
+    public static void isBlank(String obj, Integer code, String msg) {
+        if (StringUtils.isBlank(obj)) {
+            throw new SesMobileRpsException(code, msg);
+        }
+    }
+
+    /**
+     * collection is empty throw SesMobileRpsException
+     * @param collection
+     * @param code
+     * @param msg
+     */
+    public static void isEmpty(Collection collection, Integer code, String msg) {
+        if (collection.isEmpty()) {
+            throw new SesMobileRpsException(code, msg);
+        }
+    }
+
+    /**
+     * flag is false throw SesMobileRpsException
+     * @param flag
+     * @param code
+     * @param msg
+     */
+    public static void isTrue(Boolean flag, Integer code, String msg) {
+        if (!flag) {
+            throw new SesMobileRpsException(code, msg);
+        }
+    }
+
+}
