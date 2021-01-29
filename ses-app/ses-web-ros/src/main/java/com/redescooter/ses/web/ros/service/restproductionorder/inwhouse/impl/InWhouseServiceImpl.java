ef6715f6a06130004ab7086baf6adc8b0e916a7c
chris <chris@redescooter.com>
2021-01-29 16:46:52 +0800

    fix:创建入库单注释

--- ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/service/restproductionorder/inwhouse/impl/InWhouseServiceImpl.java
+++ ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/service/restproductionorder/inwhouse/impl/InWhouseServiceImpl.java
@@ -165,25 +165,24 @@ public class InWhouseServiceImpl implements InWhouseService {
         // 处理子表
         createInWhouseB(inWhouseOrder, enter);
         // 操作记录
-        SaveOpTraceEnter opTraceEnter = new SaveOpTraceEnter(null, inWhouseOrder.getId(), OrderTypeEnums.FACTORY_INBOUND.getValue(), OrderOperationTypeEnums.CREATE.getValue(),
-                inWhouseOrder.getRemark());
+        SaveOpTraceEnter opTraceEnter = new SaveOpTraceEnter(null, inWhouseOrder.getId(), OrderTypeEnums.FACTORY_INBOUND.getValue(), OrderOperationTypeEnums.CREATE.getValue(), inWhouseOrder.getRemark());
         opTraceEnter.setUserId(enter.getUserId());
         productionOrderTraceService.save(opTraceEnter);
         // 状态流转
-        OrderStatusFlowEnter orderStatusFlowEnter = new OrderStatusFlowEnter(null, inWhouseOrder.getInWhStatus(), OrderTypeEnums.FACTORY_INBOUND.getValue(), inWhouseOrder.getId(),
-                inWhouseOrder.getRemark());
+        OrderStatusFlowEnter orderStatusFlowEnter = new OrderStatusFlowEnter(null, inWhouseOrder.getInWhStatus(), OrderTypeEnums.FACTORY_INBOUND.getValue(), inWhouseOrder.getId(), inWhouseOrder.getRemark());
         orderStatusFlowEnter.setUserId(enter.getUserId());
         orderStatusFlowService.save(orderStatusFlowEnter);
         try {
             // 如果是从仓库新增的入库单 新增就是待入库状态
-            if (1 == enter.getIfWh()) {
+            /*if (1 == enter.getIfWh()) {
                 wmsMaterialStockService.waitInStock(inWhouseOrder.getOrderType(), inWhouseOrder.getId(), inWhouseOrder.getCountryType(), enter.getUserId());
             } else {
                 // 如果是整车入库单或者是组装件入库单 生成的时候就是待入库状态 这个时候 要处理库存
                 if (inWhouseOrder.getOrderType() == 1 || inWhouseOrder.getOrderType() == 2) {
                     wmsMaterialStockService.waitInStock(inWhouseOrder.getOrderType(), inWhouseOrder.getId(), inWhouseOrder.getCountryType(), enter.getUserId());
                 }
-            }
+            }*/
+            wmsMaterialStockService.waitInStock(inWhouseOrder.getOrderType(), inWhouseOrder.getId(), inWhouseOrder.getCountryType(), enter.getUserId());
         } catch (Exception e) {
 
         }
