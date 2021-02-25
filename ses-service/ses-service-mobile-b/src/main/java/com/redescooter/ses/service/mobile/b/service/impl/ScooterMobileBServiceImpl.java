f203c056df7708ee60ca7d08cd0844a27c57abf7
aleks <aleks@redescooter.com>
2021-02-25 15:11:21 +0800

    fix：分布式异常没有处理 查询用户绑定的车辆信息时 抛出的异常 上个系统接不到 改为返回null

--- ses-service/ses-service-mobile-b/src/main/java/com/redescooter/ses/service/mobile/b/service/impl/ScooterMobileBServiceImpl.java
+++ ses-service/ses-service-mobile-b/src/main/java/com/redescooter/ses/service/mobile/b/service/impl/ScooterMobileBServiceImpl.java
@@ -53,8 +53,10 @@ public class ScooterMobileBServiceImpl implements ScooterMobileBService {
          * 检查当前用户是否分配车辆
          */
         if (null == scooter) {
-            throw new MobileBException(ExceptionCodeEnums.DRIVER_NOT_ASSIGNED_VEHICLE.getCode(),
-                    ExceptionCodeEnums.DRIVER_NOT_ASSIGNED_VEHICLE.getMessage());
+//            throw new MobileBException(ExceptionCodeEnums.DRIVER_NOT_ASSIGNED_VEHICLE.getCode(),
+//                    ExceptionCodeEnums.DRIVER_NOT_ASSIGNED_VEHICLE.getMessage());
+            // todo 分布式异常没有处理  抛出的异常 上个系统接不到 改为返回null
+            return null;
         }
 
         /**
