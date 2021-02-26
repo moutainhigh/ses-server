6669100cd3209f7486aaf120562aa1ad2fce93f8
aleks <aleks@redescooter.com>
2021-02-26 11:21:02 +0800

    fix：app获取个人详情的接口区分Tob还是ToC  分别调不同的模块

--- ses-app/ses-mobile-client/src/main/java/com/redescooter/ses/mobile/client/controller/UserProfileMobileController.java
+++ ses-app/ses-mobile-client/src/main/java/com/redescooter/ses/mobile/client/controller/UserProfileMobileController.java
@@ -1,20 +1,28 @@
 package com.redescooter.ses.mobile.client.controller;
 
+import com.redescooter.ses.api.common.enums.user.UserServiceTypeEnum;
 import com.redescooter.ses.api.common.vo.base.GeneralEnter;
 import com.redescooter.ses.api.common.vo.base.GeneralResult;
+import com.redescooter.ses.api.common.vo.base.IdEnter;
 import com.redescooter.ses.api.common.vo.base.Response;
+import com.redescooter.ses.api.hub.service.customer.ConsumerUserProfileService;
+import com.redescooter.ses.api.hub.vo.QueryUserProfileResult;
 import com.redescooter.ses.api.mobile.b.service.UserProfileMobileService;
 import com.redescooter.ses.api.mobile.b.vo.SaveUserProfileEnter;
 import com.redescooter.ses.api.mobile.b.vo.UserProfileResult;
+import com.redescooter.ses.mobile.client.config.UserComponent;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboReference;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @ClassName:UserProfileController
@@ -33,10 +41,31 @@ public class UserProfileMobileController {
     @DubboReference
     private UserProfileMobileService userProfileMobileService;
 
+    @Resource
+    private UserComponent userComponent;
+
+    @DubboReference
+    private ConsumerUserProfileService consumerUserProfileService;
+
     @ApiOperation(value = "个人信息")
     @RequestMapping(value = "/detail")
     public Response<UserProfileResult> userProfile(@ModelAttribute GeneralEnter enter) {
-        return new Response<>(userProfileMobileService.userProfile(enter));
+        Integer userServiceType = userComponent.getUserServiceTypeById(enter);
+        UserProfileResult profileResult = new UserProfileResult();
+        if (UserServiceTypeEnum.B.getType().equals(userServiceType)) {
+            log.info("用户类型为ToB");
+            profileResult = userProfileMobileService.userProfile(enter);
+        } else if (UserServiceTypeEnum.C.getType().equals(userServiceType)) {
+            log.info("用户类型为ToC");
+            IdEnter idEnter = new IdEnter();
+            BeanUtils.copyProperties(enter, idEnter);
+            idEnter.setId(enter.getUserId());
+            QueryUserProfileResult toCProfile = consumerUserProfileService.queryUserProfile(idEnter);
+            if (toCProfile != null) {
+                BeanUtils.copyProperties(toCProfile, profileResult);
+            }
+        }
+        return new Response<>(profileResult);
     }
 
     @ApiOperation(value = "个人信息修改")
