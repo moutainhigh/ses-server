b0ce6d15f3fb393e88a9555bebe22a10a7393e0c
chris <chris@redescooter.com>
2021-03-16 15:16:10 +0800

    feat:site_parts表自动生成

---
+++ ses-api/ses-api-hub/src/main/java/com/redescooter/ses/api/hub/service/website/PartsService.java
@@ -0,0 +1,14 @@
+package com.redescooter.ses.api.hub.service.website;
+
+/**
+ * @Description 销售配件数据同步到官网的接口
+ * @Author Chris
+ * @Date 2021/3/16 13:28
+ */
+public interface PartsService {
+
+
+
+
+
+}
---
+++ ses-service/ses-service-hub/src/main/java/com/redescooter/ses/service/hub/source/website/service/impl/PartsServiceImpl.java
@@ -0,0 +1,19 @@
+package com.redescooter.ses.service.hub.source.website.service.impl;
+
+import com.redescooter.ses.api.hub.service.website.PartsService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.annotation.DubboService;
+
+/**
+ * @Description 销售配件数据同步到官网的实现类
+ * @Author Chris
+ * @Date 2021/3/16 13:30
+ */
+@DubboService
+@Slf4j
+public class PartsServiceImpl implements PartsService {
+
+
+
+
+}
