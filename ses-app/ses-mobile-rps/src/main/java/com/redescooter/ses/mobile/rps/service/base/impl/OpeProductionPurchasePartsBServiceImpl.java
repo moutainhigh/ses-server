3b6dc39995db667ef6f93bcb321209ae3b365517
chris <chris@redescooter.com>
2021-03-23 17:39:48 +0800

    fix:update transactional tag

--- ses-app/ses-mobile-rps/src/main/java/com/redescooter/ses/mobile/rps/service/base/impl/OpeProductionPurchasePartsBServiceImpl.java
+++ ses-app/ses-mobile-rps/src/main/java/com/redescooter/ses/mobile/rps/service/base/impl/OpeProductionPurchasePartsBServiceImpl.java
@@ -4,12 +4,12 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.redescooter.ses.mobile.rps.dao.base.OpeProductionPurchasePartsBMapper;
 import com.redescooter.ses.mobile.rps.dm.OpeProductionPurchasePartsB;
 import com.redescooter.ses.mobile.rps.service.base.OpeProductionPurchasePartsBService;
+import io.seata.spring.annotation.GlobalTransactional;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Transactional
+@GlobalTransactional
 @Service
 public class OpeProductionPurchasePartsBServiceImpl extends ServiceImpl<OpeProductionPurchasePartsBMapper, OpeProductionPurchasePartsB> implements OpeProductionPurchasePartsBService {
 
