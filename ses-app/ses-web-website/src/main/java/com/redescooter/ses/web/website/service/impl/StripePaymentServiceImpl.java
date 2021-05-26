fafeada995a8b772622c5a6b2f97cd13e2369459
courtney977 <frode@redescooter.com>
2021-05-26 18:15:46 +0800

    fix:官网同步ros价格调整

--- ses-app/ses-web-website/src/main/java/com/redescooter/ses/web/website/service/impl/StripePaymentServiceImpl.java
+++ ses-app/ses-web-website/src/main/java/com/redescooter/ses/web/website/service/impl/StripePaymentServiceImpl.java
@@ -455,14 +455,14 @@ public class StripePaymentServiceImpl implements StripePaymentService {
             BigDecimal a = siteOrder.getPrepaidDeposit().add(siteOrder.getFreight());
             BigDecimal add2 = new BigDecimal(siteOrder.getDef1()).divide(new BigDecimal(siteProductPrice.getInstallmentTime()), 2, BigDecimal.ROUND_UP).add(siteProductPrice.getShouldPayPeriod());
             syncOrderDataEnter.setAmountPaid(siteOrder1.getPrepaidDeposit().add(siteOrder1.getFreight()).add(add2.multiply(new BigDecimal(siteOrder1.getDef2()))));
-            syncOrderDataEnter.setAmountDiscount(siteOrder.getAmountDiscount());
+            syncOrderDataEnter.setAmountDiscount(siteOrder1.getAmountDiscount());
             syncOrderDataEnter.setAmountObligation(siteOrder1.getAmountObligation().multiply(new BigDecimal(siteProductPrice.getInstallmentTime()).subtract(new BigDecimal(siteOrder1.getDef2()))));
             syncOrderDataEnter.setTotalPrice(syncOrderDataEnter.getAmountObligation().add(syncOrderDataEnter.getAmountPaid()));
         }else {
-            syncOrderDataEnter.setAmountPaid(siteOrder.getAmountPaid());
-            syncOrderDataEnter.setAmountDiscount(siteOrder.getAmountDiscount());
-            syncOrderDataEnter.setAmountObligation(siteOrder.getAmountObligation());
-            syncOrderDataEnter.setTotalPrice(siteOrder.getTotalPrice());
+            syncOrderDataEnter.setAmountPaid(siteOrder1.getAmountPaid());
+            syncOrderDataEnter.setAmountDiscount(siteOrder1.getAmountDiscount());
+            syncOrderDataEnter.setAmountObligation(siteOrder1.getAmountObligation());
+            syncOrderDataEnter.setTotalPrice(siteOrder1.getTotalPrice());
         }
         syncOrderDataEnter.setPayStatus(siteOrder.getPayStatus());
         syncOrderDataEnter.setIsInstallment(sitePaymentType.getPaymentCode());
