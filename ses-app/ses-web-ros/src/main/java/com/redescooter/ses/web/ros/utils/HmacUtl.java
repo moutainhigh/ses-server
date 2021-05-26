4391f83b6427b7b92dffcc600a0a62792ad60ac3
Charles <charles@redescooter.com>
2021-05-26 17:49:41 +0800

    feat sim 加密获取密钥

---
+++ ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/utils/HmacUtl.java
@@ -0,0 +1,93 @@
+package com.redescooter.ses.web.ros.utils;
+
+import com.google.common.collect.Maps;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Map;
+
+/**
+ * @ClassName HmacUtl
+ * @Description sim 加密
+ * @Author Charles
+ * @Date 2021/05/26 17:25
+ */
+public class HmacUtl {
+
+    /**
+     * MAC 算法
+     */
+    static final String ALGORITHM_MAC = "HmacSHA1";
+
+    static final String SIM_HASH = "hash";
+
+    static final String SIM_TIMESTAMP = "timestamp";
+
+    /**
+     * 密钥
+     */
+    @Value("${sim.api_shared_key}")
+    static String API_SHARED_KEY;
+
+    @Value("${sim.account_uuid}")
+    static String ACCOUNT_UUID;
+
+    @Value("${sim.user_uuid}")
+    static String USER_UUID;
+
+    @Value("${sim.api_link}")
+    static String API_LINK;
+
+    /**
+     * @Title: generateKey
+     * @Description: // 生成密钥
+     * @Param: []
+     * @Return: java.util.Map<java.lang.String, java.lang.String>
+     * @Date: 2021/5/26 5:34 下午
+     * @Author: Charles
+     */
+    public static Map<String, String> generateKey() {
+        try {
+            Map<String, String> map = Maps.newHashMap();
+            long l = System.currentTimeMillis() / 1000;
+            String timestamp = String.format("%010d", l);
+            String hash = encodeBase64(encryptionHMAC(timestamp + ACCOUNT_UUID));
+            map.put(SIM_TIMESTAMP, timestamp);
+            map.put(SIM_HASH, hash);
+            return map;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * @Title: encryptionHMAC
+     * @Description: // HMAC加密
+     * @Param: [source]
+     * @Return: byte[]
+     * @Date: 2021/5/26 5:34 下午
+     * @Author: Charles
+     */
+    static byte[] encryptionHMAC(String source) throws Exception {
+        SecretKey secretKey = new SecretKeySpec(API_SHARED_KEY.getBytes("UTF-8"), ALGORITHM_MAC);
+        Mac mac = Mac.getInstance(ALGORITHM_MAC);
+        mac.init(secretKey);
+        mac.update(source.getBytes("UTF-8"));
+        return mac.doFinal();
+    }
+
+    /**
+     * @Title: encodeBase64
+     * @Description: // base64编码
+     * @Param: [source]
+     * @Return: java.lang.String
+     * @Date: 2021/5/26 5:34 下午
+     * @Author: Charles
+     */
+    static String encodeBase64(byte[] source) throws Exception {
+        return new String(Base64.encodeBase64(source), "UTF-8");
+    }
+}
