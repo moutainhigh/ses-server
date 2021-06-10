45781d2f97a8e660521ceb82870ab972db8b5f83
Charles <charles@redescooter.com>
2021-06-10 10:32:13 +0800

    fix 代码优化，判空规范

--- ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/constant/StringManaConstant.java
+++ ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/constant/StringManaConstant.java
@@ -13,4 +13,49 @@ public class StringManaConstant {
      * 国内时区
      */
     public static final String GMT_TIME_ZONE = "+08:00";
+
+    /**************************** Response parameter start **************************/
+
+    public static final String RESPONSE_STATUS_SUCESS = "sucess";
+
+    public static final String RESPONSE_ACK = "ack";
+
+    public static final String RESPONSE_TOTAL = "total";
+
+    public static final String RESPONSE_DATA = "data";
+    /**************************** Response parameter  end  **************************/
+
+    /**************************** SQL start  **************************/
+    /**
+     * 查询一条
+     */
+    public static final String SQL_LAST_LIMIT_ONE = "limit 1";
+
+    /**************************** SQL  end   **************************/
+
+
+    /**
+     * 值为null，返回true
+     * @param obj
+     * @return
+     */
+    public static boolean entityIsNull(Object obj) {
+        if (null == obj) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 值为null，返回false
+     * @param obj
+     * @return
+     */
+    public static boolean entityIsNotNull(Object obj) {
+        if (null != obj) {
+            return true;
+        }
+        return false;
+    }
+
 }
--- ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/utils/NumberUtil.java
+++ ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/utils/NumberUtil.java
@@ -14,6 +14,19 @@ import org.apache.commons.lang.StringUtils;
 @Log4j
 public class NumberUtil {
 
+    static final Integer NUM_ZERO = 0;
+
+    static final Integer NUM_TWO = 2;
+
+    static final Integer NUM_TEN = 10;
+
+    static final Integer NUM_TWENTY = 20;
+
+    static final Integer NUM_FORTY = 40;
+
+    static final Integer NUM_FIFTY = 50;
+
+
     public static int getStrToNum(String str) {
         int num = 0;
         if (StringUtils.isBlank(str)) {
@@ -38,4 +51,77 @@ public class NumberUtil {
         }
         return num;
     }
+
+    /**
+     * 小于2 或者 大于50
+     * @param num
+     * @return
+     */
+    public static boolean ltTwoOrGtFifty(int num) {
+        if (NUM_TWO > num || NUM_FIFTY < num) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 小于2 或者大于40
+     * @param num
+     * @return
+     */
+    public static boolean ltTwoOrGtForty(int num){
+        if (NUM_TWO > num || NUM_FORTY < num) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 小于2 或者 大于 20
+     * @param num
+     * @return
+     */
+    public static boolean ltTwoOrGtTwenty(int num){
+        if (NUM_TWO > num || NUM_TWENTY < num) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 小于2 或者 大于10
+     * @param num
+     * @return
+     */
+    public static boolean ltTwoOrGtTen(int num) {
+        if (NUM_TWO > num || NUM_TEN < num) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 不等于null 并且 大于50
+     * @param param
+     * @return
+     */
+    public static boolean notNullAndGtFifty(String param) {
+        if (null != param && NUM_FIFTY < param.length()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 等于0
+     * @param num
+     * @return
+     */
+    public static boolean eqZero(int num) {
+        if (NUM_ZERO == num) {
+            return true;
+        }
+        return false;
+    }
+
 }
