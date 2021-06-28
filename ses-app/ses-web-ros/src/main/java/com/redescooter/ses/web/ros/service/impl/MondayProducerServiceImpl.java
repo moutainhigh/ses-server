49a053358d272d3e653900caeb13edb0024f8835
Charles <charles@redescooter.com>
2021-06-28 15:18:21 +0800

    fix monday优化，异步

--- ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/service/impl/MondayProducerServiceImpl.java
+++ ses-app/ses-web-ros/src/main/java/com/redescooter/ses/web/ros/service/impl/MondayProducerServiceImpl.java
@@ -19,12 +19,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * @ClassName MondayRecordServiceImpl
@@ -50,53 +50,55 @@ public class MondayProducerServiceImpl implements MondayProducerService {
 
     private Map<String, String> map = MondayRedeEnums.getMondayRedeTitle();
 
-    @Async
     @Override
     public void save(Long userId, String scooterResult, String email, String telphone) {
-        ScoScooterResult scooter = JSONObject.parseObject(scooterResult, ScoScooterResult.class);
-        log.info("——————————————————————执行monday相关数据  start——————————————————————");
-        if (null == scooter) {
-            log.error("scoScooter data is null,mondayRecord data save fail ");
-            return;
-        }
+        CompletableFuture noReturn = CompletableFuture.runAsync(()->{
+            ScoScooterResult scooter = JSONObject.parseObject(scooterResult, ScoScooterResult.class);
+            log.info("scooter 数据{}_______", scooter);
+            log.info("——————————————————————执行monday相关数据  start——————————————————————");
+            if (null == scooter) {
+                log.error("scooter data is null,mondayRecord data save fail ");
+                return;
+            }
 
-        log.info("——————————————————————上传 monday start——————————————————————");
-        log.info("monday 板名 {}", mondayConfig.getCustomerBoardName());
-        String boardId = mondayService.existBoardName(mondayConfig.getCustomerBoardName());
-        // 创建行
-        log.info("——————————————————————创建行 monday start——————————————————————组名{}", mondayConfig.getCustomerGroupName());
-        MondayItem item = new MondayItem();
-        item.setBoardId(boardId);
-        item.setGroupId(mondayConfig.getCustomerGroupName());
-        item.setItemName(JSON.toJSONString(DateUtil.getDateTime()));
-        String itemId = mondayService.insertItem(item);
-        log.info("——————————————————————创建行 monday end——————————————————————{}", itemId);
-        String[] split = scooter.getBattery().split(",");
-        String replace = scooter.getBattery().replace(",", "\n");
-        String tableSn = scooter.getTabletSn() + "5";
-        // 数据
-        List<MondayData> list = new ArrayList<>();
-        list.add(new MondayData(MondayRedeEnums.E50_OU_E100.getId(), ScooterModelEnum.getScooterModelByType(Integer.parseInt(scooter.getModel()))));
-        list.add(new MondayData(MondayRedeEnums.NOMBRE_DE_BATTERIE.getId(), String.valueOf(split.length)));
-        list.add(new MondayData(MondayRedeEnums.N_DE_SERIE_DE_LA_BATTERIE.getId(), replace));
-        list.add(new MondayData(MondayRedeEnums.ID_DASHBOARD.getId(), tableSn));
-        list.add(new MondayData(MondayRedeEnums.EMAIL.getId(), email));
-        list.add(new MondayData(MondayRedeEnums.TELEPHONE.getId(), telphone));
-        mondayService.batchItemData(list, boardId, itemId);
-        log.info("——————————————————————上传 monday end——————————————————————");
+            log.info("——————————————————————上传 monday start——————————————————————");
+            log.info("monday 板名 {}", mondayConfig.getCustomerBoardName());
+            String boardId = mondayService.existBoardName(mondayConfig.getCustomerBoardName());
+            // 创建行
+            log.info("——————————————————————创建行 monday start——————————————————————组名{}", mondayConfig.getCustomerGroupName());
+            MondayItem item = new MondayItem();
+            item.setBoardId(boardId);
+            item.setGroupId(mondayConfig.getCustomerGroupName());
+            item.setItemName(JSON.toJSONString(DateUtil.getDateTime()));
+            String itemId = mondayService.insertItem(item);
+            log.info("——————————————————————创建行 monday end——————————————————————{}", itemId);
+            String[] split = scooter.getBattery().split(",");
+            String replace = scooter.getBattery().replace(",", "\n");
+            String tableSn = scooter.getTabletSn() + "5";
+            // 数据
+            List<MondayData> list = new ArrayList<>();
+            list.add(new MondayData(MondayRedeEnums.E50_OU_E100.getId(), ScooterModelEnum.getScooterModelByType(Integer.parseInt(scooter.getModel()))));
+            list.add(new MondayData(MondayRedeEnums.NOMBRE_DE_BATTERIE.getId(), String.valueOf(split.length)));
+            list.add(new MondayData(MondayRedeEnums.N_DE_SERIE_DE_LA_BATTERIE.getId(), replace));
+            list.add(new MondayData(MondayRedeEnums.ID_DASHBOARD.getId(), tableSn));
+            list.add(new MondayData(MondayRedeEnums.EMAIL.getId(), email));
+            list.add(new MondayData(MondayRedeEnums.TELEPHONE.getId(), telphone));
+            mondayService.batchItemData(list, boardId, itemId);
+            log.info("——————————————————————上传 monday end——————————————————————");
 
-        MondayRecord mondayRecord = new MondayRecord();
-        mondayRecord.setId(idAppService.getId(SequenceName.MONDAY_RECORD));
-        mondayRecord.setWorkspaceId(mondayConfig.getWorkspaceId());
-        mondayRecord.setBoardId(boardId);
-        mondayRecord.setItemId(itemId);
-        mondayRecord.setGroupName(mondayConfig.getCustomerGroupName());
-        mondayRecord.setColumnNames(JSON.toJSONString(map.values()));
-        mondayRecord.setItemValue(JSON.toJSONString(list));
-        mondayRecord.setRsn(scooter.getTabletSn());
-        mondayRecord.setCreatedBy(userId);
-        mondayRecord.setCreatedTime(new Date());
-        recordMapper.insert(mondayRecord);
-        log.info("——————————————————————执行monday相关数据  end——————————————————————");
+            MondayRecord mondayRecord = new MondayRecord();
+            mondayRecord.setId(idAppService.getId(SequenceName.MONDAY_RECORD));
+            mondayRecord.setWorkspaceId(mondayConfig.getWorkspaceId());
+            mondayRecord.setBoardId(boardId);
+            mondayRecord.setItemId(itemId);
+            mondayRecord.setGroupName(mondayConfig.getCustomerGroupName());
+            mondayRecord.setColumnNames(JSON.toJSONString(map.values()));
+            mondayRecord.setItemValue(JSON.toJSONString(list));
+            mondayRecord.setRsn(scooter.getTabletSn());
+            mondayRecord.setCreatedBy(userId);
+            mondayRecord.setCreatedTime(new Date());
+            recordMapper.insert(mondayRecord);
+            log.info("——————————————————————执行monday相关数据  end——————————————————————");
+        });
     }
 }
